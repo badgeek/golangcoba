3afa44f0b79091d457c363d2924a785706f3a7d6
Budi Prakosa <budi.prakosa@codemi.co.id>
2020-11-01 10:16:41 +0700

    refactor routing

--- main.go
+++ main.go
@@ -13,13 +13,17 @@ import (
 
 // var app appstate.AppState
 
-func setupHTTP() {
+func setupRouting() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/versions", controller.ShowVersions)
 	mux.HandleFunc("/versions/create", controller.Create)
 	mux.HandleFunc("/versions/list", controller.ListVersion)
 	mux.HandleFunc("/versions/listrate", controller.ListRate)
-	err := http.ListenAndServe(":8000", mux)
+	return mux
+}
+
+func setupHTTP(route *http.ServeMux) {
+	err := http.ListenAndServe(":8000", route)
 	log.Fatal(err)
 }
 
@@ -36,35 +40,8 @@ func main() {
 	appstate.SetupLog()
 	appstate.SetupDB(os.Getenv("DBCONFIG"))
 	appstate.ConnectDB()
-
-	// appstate.App.LogInfo = new log.l
-
-	appstate.App.LogInfo.Printf("coba coba")
-
+	appstate.App.LogInfo.Printf("SERVER: Started")
 	defer appstate.CloseDB()
-	setupHTTP()
-
-	// c, err := semver.NewConstraint("<= 1.2.3, >= 1.4")
-	// if err != nil {
-	// 	// Handle constraint not being parseable.
-	// }
-
-	// v, err := semver.NewVersion("1.3")
-	// if err != nil {
-	// 	// Handle version not being parseable.
-	// }
-
-	// incp := v.IncMinor()
-	// fmt.Println(incp)
-
-	// // Validate a version against a constraint.
-	// _, msgs := c.Validate(v)
-	// // a is false
-	// for _, m := range msgs {
-	// 	fmt.Println(m)
-
-	// 	// Loops over the errors which would read
-	// 	// "1.3 is greater than 1.2.3"
-	// 	// "1.3 is less than 1.4"
-	// }
+	route := setupRouting()
+	setupHTTP(route)
 }
